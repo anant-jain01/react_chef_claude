d1f565d5f74f10c5733589286473deea7d07bf68
anant-jain01 <jain.anant609@gmail.com>
2025-03-28 22:32:57 +0530

    modified:   compoenents/Main.jsx

--- compoenents/Main.jsx
+++ compoenents/Main.jsx
@@ -1,17 +1,23 @@
 export default function Main() {
-  const ingredients=["panner",'oregan','tomato'];
-  const perform=ingredients.map((data)=>{
-    return <li key="used">{data}</li>
-  });
-  function submit(){
+  const ingredients = [];
+  function submit(event) {
+    event.preventDefault();
     console.log("submitted");
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    ingredients.push(newIngredient);
+    console.log(ingredients);
   }
+  const perform = ingredients.map((data) => {
+    return <li key="used">{data}</li>;
+  });
   return (
     <main>
       <form onSubmit={submit} className="add-ingredient-form">
         <input
           aria-label="Add ingredient"
           type="text"
+          name="ingredient"
           placeholder="e.g. oregano"
         />
         <button>Add Ingredient</button>
