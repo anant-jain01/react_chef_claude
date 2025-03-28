69a990549c7baa949b0c8b3fb13aeb1d2f4f65b7
anant-jain01 <jain.anant609@gmail.com>
2025-03-28 21:54:16 +0530

    modified:   compoenents/Header.jsx

--- compoenents/Header.jsx
+++ compoenents/Header.jsx
@@ -1,8 +1,11 @@
 export default function Header() {
+  function lala(){
+    console.log("hovered")
+  }
   return (
     <header>
       <img src="../images/chef-claude-icon.png" alt="" />
-      <h1>Chef Claude</h1>
+      <h1 onMouseOver={lala}>Chef Claude</h1>
     </header>
   );
 }
