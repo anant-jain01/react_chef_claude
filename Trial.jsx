3ad0ecb7145cb9f70996eace8a78e6a44fa7de5e
anant-jain01 <jain.anant609@gmail.com>
2025-03-29 09:24:19 +0530

    modified:   Trial.jsx

--- Trial.jsx
+++ Trial.jsx
@@ -1,50 +1,24 @@
-import React from "react";
-import starEmpty from "./images/star-empty.png";
-import starFilled from "./images/star-filled.png";
-import avatar from "./images/user.png";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-export default function App() {
-  const [contact, setContact] = React.useState({
-    firstName: "John",
-    lastName: "Doe",
-    phone: "+1 (212) 555-1212",
-    email: "itsmyrealname@example.com",
-    isFavorite: false,
-  });
-  let starIcon = contact.isFavorite ? starFilled : starEmpty;
-  function toggleFavorite() {
-    setContact(prev=>{
-      return{
-        ...prev,isFavorite: !prev.isFavorite
-      }
-    })
-  }
+/**
+ * Challenge: add another label and input for the password field
+ */
 
+function App() {
   return (
-    <main>
-      <article className="card">
-        <img
-          src={avatar}
-          className="avatar"
-          alt="User profile picture of John Doe"
-        />
-        <div className="info">
-          <button
-            onClick={toggleFavorite}
-            aria-pressed={contact.isFavorite}
-            aria-label={contact.isFavorite?"remove":"add"}
-            className="favorite-button"
-          >
-            <img src={starIcon} alt={contact.isFavorite?"filled star":"empty star"} className="favorite" />
-          </button>
-          <h2 className="name">
-            {contact.firstName}
-            {contact.lastName}
-          </h2>
-          <p className="contact">{contact.phone}</p>
-          <p className="contact">{contact.email}</p>
-        </div>
-      </article>
-    </main>
-  );
+    <section>
+      <h1>Signup form</h1>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" placeholder='***' />
+        
+      </form>
+    </section>
+  )
 }
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
