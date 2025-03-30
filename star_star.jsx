5fa037fe071ea776e891fb10ed7f64d2612d2a78
anant-jain01 <jain.anant609@gmail.com>
2025-03-30 19:55:44 +0530

    new file:   Star_app.jsx

---
+++ star_star.jsx
@@ -0,0 +1,22 @@
+import starEmpty from "./images/star-empty.png";
+import starFilled from "./images/star-filled.png";
+export default function Star(props) {
+  let starIcon = props.isFavorite ? starFilled : starEmpty;
+
+  return (
+    <button
+      onClick={props.handleClick}
+      aria-pressed={props.isFavorite}
+      aria-label={
+        props.isFavorite ? "Remove from favorites" : "Add to favorites"
+      }
+      className="favorite-button"
+    >
+      <img
+        src={starIcon}
+        alt={props.isFavorite ? "filled star icon" : "empty star icon"}
+        className="favorite"
+      />
+    </button>
+  );
+}
