9f6cc720d2b3c9f77f41c066d9ffcc831da3ec48
anant-jain01 <jain.anant609@gmail.com>
2025-03-30 13:42:04 +0530

    modified:   joke.jsx

--- joke.jsx
+++ joke.jsx
@@ -1,21 +1,35 @@
 import React from "react"
-export default function Joke(props) {
-  /**
-   * Challenge:
-   * - Create state `isShown` (boolean, default to `false`)
-   * - Add a button that toggles the value back and forth
-   */
-  const [isShown,setisShown]=React.useState(false);
-  console.log(isShown)
-  function func(){
-    setisShown((prev)=>!prev)
-  }
-  return (
-      <div>
-          {props.setup && <h3>{props.setup}</h3>}
-          {isShown&&<p>{props.punchline}</p>}
-          <button onClick={func}>Show or Hide</button>
-          <hr />
-      </div>
-  )
+// export default function Joke(props) {
+//   /**
+//    * Challenge:
+//    * - Create state `isShown` (boolean, default to `false`)
+//    * - Add a button that toggles the value back and forth
+//    */
+//   const [isShown,setisShown]=React.useState(false);
+//   console.log(isShown)
+//   function func(){
+//     setisShown((prev)=>!prev)
+//   }
+//   return (
+//       <div>
+//           {props.setup && <h3>{props.setup}</h3>}
+//           {isShown&&<p>{props.punchline}</p>}
+//           <button onClick={func}>Show or Hide</button>
+//           <hr />
+//       </div>
+//   )
+// }
+export default function App() {
+    const [unreadMessages, setUnreadMessages] = React.useState(["a", "b"])
+    
+    /**
+     * Challenge:
+     * Only display the <h1> below if there are unread messages
+     */
+    
+    return (
+        <div>
+            {unreadMessages.length>0 &&<h1>You have {unreadMessages.length} unread messages!</h1>}
+        </div>
+    )
 }
