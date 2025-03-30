862029ba7f00f2131c1e1955ccb7a4f763b15adf
anant-jain01 <jain.anant609@gmail.com>
2025-03-30 14:06:47 +0530

    modified:   compoenents/Main.jsx

--- compoenents/Main.jsx
+++ compoenents/Main.jsx
@@ -1,30 +1,40 @@
-import { useState } from "react";
+import React from "react";
+
 export default function Main() {
-  const [ingredients, setingredients] = useState([]);
+  const [ingredients, setIngredients] = React.useState([]);
 
-  function submit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function addIngredient(formData) {
     const newIngredient = formData.get("ingredient");
-    setingredients((prev) => [...prev, newIngredient]);
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
   }
-
-  const perform = ingredients.map((data, index) => {
-    return <li key={index}>{data}</li>;
-  });
-
   return (
     <main>
-      <form onSubmit={submit} className="add-ingredient-form">
+      <form action={addIngredient} className="add-ingredient-form">
         <input
-          aria-label="Add ingredient"
           type="text"
-          name="ingredient"
           placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
         />
-        <button>Add Ingredient</button>
+        <button>Add ingredient</button>
       </form>
-      <ul>{perform}</ul>
+      {ingredients.length>0 &&<section>
+        <h2>Ingredients on hand:</h2>
+        <ul className="ingredients-list" aria-live="polite">
+          {ingredientsListItems}
+        </ul>
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button>Get a recipe</button>
+        </div>
+      </section>}
     </main>
   );
 }
